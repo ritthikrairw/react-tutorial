50aad72393d63a1eabb4b601b1ffa486f02f6d0f
Ritthikrai (Champ) W <ritthikrai.rw@gmail.com>
2025-04-25 14:47:11 +0700

    🎨 Adjust structure

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
   base: './',
